b40226a2001b3836cfab868569095e2b543aa044
Anchor <netuser.orz@icloud.com>
2018-04-17 13:18:59 +0800

    修改config-server启动类

--- springcloud-config-server/src/main/java/cc/zhaxin/springcloud/config/ConfigServerApplication.java
+++ springcloud-config-server/src/main/java/cc/zhaxin/springcloud/config/ConfigServerApplication.java
@@ -2,8 +2,17 @@ package cc.zhaxin.springcloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @Author: 阁楼麻雀
+ * @Date: 2018/4/17 下午1:16
+ * @Desc: config启动类
+ */
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
