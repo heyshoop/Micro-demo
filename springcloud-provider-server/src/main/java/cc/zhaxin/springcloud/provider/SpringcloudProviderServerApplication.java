02c6d0ab1efd02733f02cdddcb7d9809dcc2085e
Anchor <netuser.orz@icloud.com>
2018-04-17 13:27:04 +0800

    添加服务提供者provider-server

---
+++ springcloud-provider-server/src/main/java/cc/zhaxin/springcloud/provider/SpringcloudProviderServerApplication.java
@@ -0,0 +1,12 @@
+package cc.zhaxin.springcloud.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudProviderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudProviderServerApplication.class, args);
+    }
+}
