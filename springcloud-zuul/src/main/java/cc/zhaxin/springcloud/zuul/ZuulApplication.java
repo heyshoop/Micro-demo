3f3e02844f6e167c96f1c641a6158f1e9254d346
Anchor <netuser.orz@icloud.com>
2018-04-18 17:01:21 +0800

    归档zuul路由模块代码

--- springcloud-zuul/src/main/java/cc/zhaxin/springcloud/zuul/ZuulApplication.java
+++ springcloud-zuul/src/main/java/cc/zhaxin/springcloud/zuul/ZuulApplication.java
@@ -2,8 +2,19 @@ package cc.zhaxin.springcloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @Author: 阁楼麻雀
+ * @Date: 2018/4/18 下午4:17
+ * @Desc: zuul启动类
+ */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@RefreshScope
 public class ZuulApplication {
 
     public static void main(String[] args) {
