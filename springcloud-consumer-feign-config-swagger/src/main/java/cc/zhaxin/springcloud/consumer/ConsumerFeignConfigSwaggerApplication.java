ef25c3e17bc00fbe40f5ea16a34b5a7f47aa343d
Anchor <netuser.orz@icloud.com>
2018-04-19 11:50:10 +0800

    添加基于feign的消费者服务模块

---
+++ springcloud-consumer-feign-config-swagger/src/main/java/cc/zhaxin/springcloud/consumer/ConsumerFeignConfigSwaggerApplication.java
@@ -0,0 +1,12 @@
+package cc.zhaxin.springcloud.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerFeignConfigSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerFeignConfigSwaggerApplication.class, args);
+    }
+}
